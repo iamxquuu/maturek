4bc39a77a8f963a63517c3d8e3bcbab8ca99c7cf
marcel <marcel.maciaszek@icloud.com>
2023-04-21 20:13:53 +0200

    Initial Commit

---
+++ maturek/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  maturek
+//
+//  Created by Marcel Maciaszek on 21/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ maturek/maturekApp.swift
@@ -0,0 +1,17 @@
+//
+//  maturekApp.swift
+//  maturek
+//
+//  Created by Marcel Maciaszek on 21/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct maturekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
