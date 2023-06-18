0f5cd3441e7b19c23693578f1bc4b59f230c28c7
marcel <marcel.maciaszek@icloud.com>
2023-06-18 17:33:20 +0200

    dodane ikony svg logo popularnych social mediow kafelek social media z automatycznym przeniesieniem do danej platformy po klknieciu

--- maturek/Views/ekran startowy/Centrum.swift
+++ maturek/Views/ekran startowy/Centrum.swift
@@ -36,11 +36,11 @@ struct Centrum: View {
                         } label: {
                             ZStack {
                                 RoundedRectangle(cornerRadius: 15)
-                                    .foregroundColor(.black)
+                                    .foregroundColor(.white)
                                     .padding(.horizontal)
                                     .frame(height: 60)
                                 HStack {
-                                    Text("Zasubskrybuj MATUREK+")
+                                    Text("Subskrypcja MATUREK+")
                                         .font(.headline)
                                     
                                 }
@@ -49,12 +49,71 @@ struct Centrum: View {
                         secondSubtitle(title: "Nasze Social Media")
                     // MARK: social media
                         Group{
-                            //Twitter
-                            Link("Twitter", destination: URL(string: "https://twitter.com/maturekpl")!)
-                            //Youtube
-                            Link("Youtube", destination: URL(string: "https://www.youtube.com/@maturek")!)
-                            //Instagram
-                            Link("Instagram", destination: URL(string: "https://www.instagram.com/maturekpl")!)
+                            ZStack {
+                                reusableRectangle(cornerRadius: 15, height: 60)
+                                    .foregroundColor(.white)
+                                HStack {
+                                    //Twitter
+                                    let fixedColumns = Array(repeating: GridItem(.flexible(minimum: 0), spacing: 5, alignment: .center), count: 4)
+                                        
+                                    LazyVGrid(columns: fixedColumns) {
+                                        Link(destination: URL(string: "https://twitter.com/maturekpl")!) {
+                                            HStack {
+                                                Image("twitter")
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(height: 30)
+                                            }
+                                        }
+                                        //Youtube
+                                        Link(destination: URL(string: "https://www.youtube.com/@maturek")!) {
+                                            HStack {
+                                                Image("youtube")
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(height: 38)
+                                            }
+                                        }
+                                        //Instagram
+                                        Link(destination: URL(string: "https://www.instagram.com/maturekpl")!) {
+                                            HStack {
+                                                Image("instagram")
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(height: 30)
+                                            }
+                                        }
+                                        //tiktok
+                                        Link(destination: URL(string: "https://www.tiktok.com/@maturekpl")!) {
+                                            HStack {
+                                                Image("tiktok")
+                                                    .resizable()
+                                                    .aspectRatio(contentMode: .fit)
+                                                    .frame(height: 30)
+                                               
+                                                
+                                            }
+                                            
+                                        }
+                                    }
+                                    
+                                    
+                                    
+                                    Spacer()
+                                    
+                                    
+                                }.padding(.horizontal)
+                                
+                            }
+                            HStack {
+                                Text("*możesz kliknąć w ikonkę, aby przejść do wybranej platformy :))")
+                                    .foregroundColor(.blue)
+                                    .font(.subheadline)
+                                    .padding(.leading, 20)
+                                Spacer()
+                            }
+                            
+                                
                         }
                         
                         
